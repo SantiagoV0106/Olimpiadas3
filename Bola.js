8906b0a2ba7da538552aba70988a242a304cf566
SantiagoV0106 <88668447+SantiagoV0106@users.noreply.github.com>
2021-11-18 00:27:09 -0500

    Quita bloques

---
+++ Bola.js
@@ -0,0 +1,11 @@
+class Bola{
+    constructor(x,y){
+      this.x = x;
+      this.y = y;
+    }
+    show(){
+      fill(230);
+      ellipse(this.x, this.y, 20,20);
+    }
+  
+  }
--- sketch.js
+++ sketch.js
@@ -4,103 +4,273 @@
 //Santiago Velazco
 
 
-let taxiCol1 = [];
-let taxiCol2 = [];
-let taxiCol3 = [];
-let taxiCol4 = [];
+class Bola {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(255);
+    ellipse(this.x, this.y, 20, 20);
+  }
 
-let car = new uber(30, 185, 30);
+}
 
-let gameOver;
-
-function setup() {
-  createCanvas(600, 400);
-
-  gameOver = false;
-  let y = 100;
-
-  //Todas las columnas de taxis
-  for (let i = 0; i < 4; i++) {
-    taxiCol1.push(new Taxi(125, y, 30, 2));
-    taxiCol2.push(new Taxi(225, y, 30, 2.5));
-    taxiCol3.push(new Taxi(325, y, 30, 1.5));
-    taxiCol4.push(new Taxi(425, y, 30, 2));
-    y+=160;
+class Barra {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(255);
+    rect(this.x, this.y, 100, 15);
   }
 }
 
-function draw() {
-  background(165);
-  let x = 75;
-  
-  //Se pinta la carretera
-  for (let i = 0; i < 4; i++) {
-    fill(123);
+class BloqueRojo {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
     noStroke();
-    rectMode(CENTER);
-    rect(x + 70, 0, 80, 900);
-    x += 100;
+    fill(148, 0, 211);
+    rect(this.x, this.y, 50, 25);
+  }
+  setX(x) {
+    this.x = x;
+  }
+  validarContacto(bolita) {
+    if (bolita.x > this.x && bolita.x < this.x + 50 &&
+      bolita.y > this.y && bolita.y < this.y + 25) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class BloqueNaranja {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(75, 0, 130);
+    rect(this.x, this.y, 50, 25);
+  }
+  validarContacto(bolita) {
+    if (bolita.x > this.x && bolita.x < this.x + 50 &&
+      bolita.y > this.y && bolita.y < this.y + 25) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class BloqueAmarillo {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(0, 255, 0);
+    rect(this.x, this.y, 50, 25);
+  }
+  validarContacto(bolita) {
+    if (bolita.x > this.x && bolita.x < this.x + 50 &&
+      bolita.y > this.y && bolita.y < this.y + 25) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class BloqueLima {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(255, 255, 0);
+    rect(this.x, this.y, 50, 25);
+  }
+  validarContacto(bolita) {
+    if (bolita.x > this.x && bolita.x < this.x + 50 &&
+      bolita.y > this.y && bolita.y < this.y + 25) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class BloqueVerde {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  show() {
+    fill(255, 127, 0);
+    rect(this.x, this.y, 50, 25);
   }
 
-  //Pinta el carro rojo
-  car.display();
-  car.move();
-
-  for (let i = 0; i < 4; i++) {
-
-    //Pinta taxis columna 1
-    taxiCol1[i].display();
-    taxiCol1[i].move();
-
-    //Pinta taxis columna 2
-    taxiCol2[i].display();
-    taxiCol2[i].move();
-
-    //Pinta taxis columna 3
-    taxiCol3[i].display();
-    taxiCol3[i].move();
-
-    //Pinta taxis columna 4
-    taxiCol4[i].display();
-    taxiCol4[i].move();
-
-    //Choque  1
-    if(taxiCol1[i].validate(car.getX(),car.getY()) == true){
-      gameOver = true;
-      location.reload();
-      car.gameOver();
+  validarContacto(bolita) {
+    if (bolita.x > this.x && bolita.x < this.x + 50 &&
+      bolita.y < 135) {
+      return true;
     }
+    return false;
+  }
+}
 
-    //Choque  2
-    if(taxiCol2[i].validate(car.getX(),car.getY()) == true){
-      gameOver = true;
-      location.reload();
-      car.gameOver();
+
+let bolita = new Bola(200, 375);
+let barra;
+let isMoving;
+let calculoX;
+let calculoY;
+let rojos = [];
+let naranjas = [];
+let amarillos = [];
+let limas = [];
+let verdes = [];
+
+function setup() {
+  createCanvas(400, 400);
+  barra = new Barra(150, 385);
+  calculoX = 1;
+  calculoY = 1;
+  isMoving = false;
+
+
+  for (let index = 0; index < 8; index++) {
+    rojos.push(new BloqueRojo(index * 50, 0));
+    naranjas.push(new BloqueNaranja(index * 50, 25));
+    amarillos.push(new BloqueAmarillo(index * 50, 50));
+    limas.push(new BloqueLima(index * 50, 75));
+    verdes.push(new BloqueVerde(index * 50, 100));
+  }
+
+}
+
+function draw() {
+  background(165, 42, 42);
+  stroke(1);
+
+
+
+  for (let index = 0; index < 8; index++) {
+
+    if (rojos[index] !== undefined) {
+      rojos[index].show();
     }
-
-    //Choque  3
-    if(taxiCol3[i].validate(car.getX(),car.getY()) == true){
-      gameOver = true;
-      location.reload();
-      car.gameOver();
+    if (naranjas[index] !== undefined) {
+      naranjas[index].show();
     }
-
-    //Choque  4
-    if(taxiCol4[i].validate(car.getX(),car.getY()) == true){
-      gameOver = true;
-      location.reload();
-      car.gameOver();
+    if (amarillos[index] !== undefined) {
+      amarillos[index].show();
     }
-  } 
+    if (limas[index] !== undefined) {
+      limas[index].show();
+    }
+    if (verdes[index] !== undefined) {
+      verdes[index].show();
+    }
+  }
+  keyPressed();
 
-  //gano
-  car.win();
-  //restar
-  car.restart();
+  if (isMoving === true) {
+    bolita.x += 2 * calculoX;
+    bolita.y -= 3 * calculoY;
+  }
+  if (bolita.y < 10) {
+    calculoY = -1;
+  } else if (bolita.x > 390) {
+    calculoX = -1;
+  } else if (bolita.x < 10) {
+    calculoX = 1;
+  }
+  if (bolita.x > barra.x && bolita.x < barra.x + 100 && bolita.y > 375) {
+    calculoY = 1;
+  }
+  if (bolita.y > 410) {
+    isMoving = false;
+    bolita.x = 200;
+    bolita.y = 375;
+  }
+  noStroke();
+  barra.show();
+  bolita.show();
 
-  //Instrucciones 
+  validarContacto();
+
+}
+
+function validarContacto() {
+  for (let index = 0; index < 8; index++) {
+    if (verdes[index] !== undefined && verdes[index].validarContacto(bolita)) {
+      console.log("Contacto!!!");
+      verdes.splice(index, 1);
+      calculoY = -1;
+      return;
+    }
+    if (limas[index] !== undefined && limas[index].validarContacto(bolita)) {
+      console.log("Contacto!!!");
+      limas.splice(index, 1);
+      calculoY = -1;
+      return;
+    }
+    if (amarillos[index] !== undefined && amarillos[index].validarContacto(bolita)) {
+      console.log("Contacto!!!");
+      amarillos.splice(index, 1);
+      calculoY = -1;
+      return;
+    }
+    if (naranjas[index] !== undefined && naranjas[index].validarContacto(bolita)) {
+      console.log("Contacto!!!");
+      naranjas.splice(index, 1);
+      calculoY = -1;
+      return;
+    }
+    if (rojos[index] !== undefined && rojos[index].validarContacto(bolita)) {
+      console.log("Contacto!!!");
+      rojos.splice(index, 1);
+      calculoY = -1;
+      return;
+    }
+  }
+}
+
+function keyPressed() {
+  let mensaje;
+  if (keyIsPressed && barra.x >= 0 && barra.x <= 300) {
+
+    switch (key) {
+      case 'a' || 'D':
+        barra.x -= 3;
+        break;
+      case 'd' || 'D':
+        barra.x += 3;
+        break;
+    }
+    if (barra.x < 0) {
+      barra.x = 0;
+    } else if (barra.x > 300) {
+      barra.x = 300;
+    }
+  }
+  if (key === ' ') {
+    isMoving = true;
+    mensaje = " ";
+  }
+
+  if (isMoving === false) {
+    mensaje = "¿Ready m8? press space ";
+  }
+
+
+
   fill(255);
-  textAlign(CENTER);
-  textSize(20);
-  text('Use keys: "a" & "d" to move uber, avoid angry taxis', 260, 388);
+  textAlign(CENTER, CENTER);
+  textSize(30);
+  text(mensaje, 200, 180)
 }
